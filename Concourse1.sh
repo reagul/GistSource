93376559fc1fd190e30cc8d7b000eed2d519e5be
reagul <reagul.2007@gmail.com>
2016-05-04 09:14:29 -0400

    Update Concourse1.sh

--- Concourse1.sh
+++ Concourse1.sh
@@ -8,18 +8,18 @@ git clone Concourse1 updated-gist
 cd updated-gist
 echo "Starting MVN Test"
 mvn clean package
-echo $(date) > bumpme
-echo "Stop"
+#echo $(date) > bumpme
+echo "@@@@@@@@@@@@@@@@@ MVN Build Completed @@@@@@@@@@@@@@@@@@"
 git config --global user.email "reagul@concourse.ci"
 git config --global user.name "reagul"
 
 git add .
-git commit -m "Bumped date"
+git commit -m "Test Completed at "$(date)" "
 ls -la
 pwd
 cd ../
+echo "LS LA ouput"
 
-echo "test message here"
 ls -la
 pwd
-echo "more msg here"
+
